76881ca0e3c1127e3e797c8e3f6d4a56bb720946
Radnoumane <radnoumane.mossabely@safrangroup.com>
2023-07-28 14:40:09 +0200

    delete todo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
-<script>
+<script lang="ts">
+	interface todo {
+		id: number;
+		description: String;
+		completed: Boolean;
+	}
 	let todos = [
 		{
 			id: 1,
@@ -6,22 +11,34 @@
 			completed: false
 		}
 	];
+	$: totalTodos = todos.length;
+	$: completedTodos = todos.filter((todo) => todo.completed);
+
 	let newItem = '';
 
 	function newTodo() {
-	
 		todos = [...todos, { id: todos.length, description: newItem, completed: false }];
 		newItem = '';
 	}
+
+	function removeTodo(todo: todo) {
+		todos = todos.filter((t) => t.id !== todo.id);
+	}
 </script>
 
+<title>Todo List</title>
 <h1>Todos list</h1>
+<h3>Number of todo: {totalTodos}</h3>
 
-<ul>
-	{#each todos as todo, index}
-		<li id={index}>{todo.description}</li>
-	{/each}
-</ul>
+{#each todos as todo, index}
+	<p>
+		<input bind:checked={todo.completed} type="checkbox" />
+		{todo.description}
+		<button on:click={removeTodo(todo)}>❌</button>
+	</p>
+{/each}
+
+{JSON.stringify(todos)}
 
 <h2>Add todo:</h2>
 <label for="todoInput">Description </label><input
