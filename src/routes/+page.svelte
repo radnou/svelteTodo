f957c25acdd3a5311c1f74a4ffa50ae4027bf374
Radnoumane <radnoumane.mossabely@safrangroup.com>
2023-07-28 13:48:23 +0200

    todov1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let todos = [
+		{
+			id: 1,
+			description: 'my first todo ',
+			completed: false
+		}
+	];
+	let newItem = '';
+
+	function newTodo() {
+	
+		todos = [...todos, { id: todos.length, description: newItem, completed: false }];
+		newItem = '';
+	}
+</script>
+
+<h1>Todos list</h1>
+
+<ul>
+	{#each todos as todo, index}
+		<li id={index}>{todo.description}</li>
+	{/each}
+</ul>
+
+<h2>Add todo:</h2>
+<label for="todoInput">Description </label><input
+	type="text"
+	bind:value={newItem}
+	placeholder="What do you want to list ?"
+/>
+<button on:click|preventDefault={newTodo}>Add todo</button>
