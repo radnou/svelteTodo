28d2a3d678f2db6a975cc85bbe0a68c2277983bf
Radnoumane <radnoumane.mossabely@safrangroup.com>
2023-08-03 14:12:27 +0200

    structure and file update based on svelte tuto

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-center">
+	<ul class="menu menu-horizontal">
+		<li>
+			<a href="/" class="link link-primary">Home</a>
+		</li>
+		<li>
+			<a href="/introduction" class="link link-primary">Introduction</a>
+		</li>
+		<li>
+			<a href="/reactivity" class="link link-primary">Reactivity</a>
+		</li>
+		<li>
+			<a href="/props" class="link link-primary">Props</a>
+		</li>
+	</ul>
+</nav>
