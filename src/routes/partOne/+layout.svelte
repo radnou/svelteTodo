600911d276fb58c44cf4121e9deda1593437157f
Radnoumane Mossabely <radnou.m@gmail.com>
2023-08-05 13:10:54 +0200

    chore: Add .idea files and partOne route

---
+++ src/routes/partOne/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '$lib/components/navbar.svelte';
+
+	let menu = [
+		{
+			path: 'partOne',
+			label: 'Part 1 : Basic Svelte'
+		},
+		{
+			path: 'partTwo',
+			label: 'Part 2 : Advanced Svelte'
+		},{
+			path: 'partThree',
+			label: 'Part 3 : Basic SvelteKit'
+		},{
+			path: 'partFour',
+			label: 'Part 4 : Advanced SvelteKit'
+		}
+	];
+</script>
+
+<Navbar {menu} />
+<slot />
